ee64cce21ee1f4a5fd77ddb7ecc4fdfcd515687a
Alex <alex@alex-dev.me>
2024-03-01 00:41:48 -0600

    Add DISCORD_WEBHOOK_URL environment variable

--- src/config/envs.ts
+++ src/config/envs.ts
@@ -4,4 +4,5 @@ import { get } from "env-var";
 export const environment = {
   PORT: get("PORT").default(3000).asPortNumber(),
   NODE_ENV: get("NODE_ENV").default("development").asString(),
+  DISCORD_WEBHOOK_URL: get("DISCORD_WEBHOOK_URL").required().asString(),
 };
