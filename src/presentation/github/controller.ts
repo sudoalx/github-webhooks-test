5b97edc37797ebadd520b2d4fafc67affd874bc8
Alex <alex@alex-dev.me>
2024-02-29 23:41:30 -0600

    Update webhookHandler in GithubController

--- src/presentation/github/controller.ts
+++ src/presentation/github/controller.ts
@@ -4,7 +4,12 @@ export class GithubController {
   constructor() {}
 
   webhookHandler = (req: Request, res: Response) => {
-    console.log(`Endpoint called!`);
-    res.json({ message: "Hello, GitHub!" });
+    const githubEvent = req.headers["x-github-event"] ?? "unknown";
+    const signature = req.headers["x-hub-signature-256"] ?? "unknown";
+    const { body: payload } = req;
+
+    console.log(JSON.stringify(payload));
+
+    res.status(202).send("Accepted!");
   };
 }
