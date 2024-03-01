b76f221e9a979752a114437f31598818dbccb20c
Alex <alex@alex-dev.me>
2024-02-29 23:41:05 -0600

    Add express.json middleware to parse JSON requests

--- src/app.ts
+++ src/app.ts
@@ -9,6 +9,7 @@ function main() {
   const app = express();
   const controller = new GithubController();
 
+  app.use(express.json());
   app.post("/api/github", controller.webhookHandler);
 
   app.listen(environment.PORT, () => {
