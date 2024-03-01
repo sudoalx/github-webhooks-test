3a78e18869300ac3f6577b15fa24d9e7102ebec7
Alex <alex@alex-dev.me>
2024-03-01 00:42:22 -0600

    Add SECRET_TOKEN to environment configuration

--- src/config/envs.ts
+++ src/config/envs.ts
@@ -5,4 +5,5 @@ export const environment = {
   PORT: get("PORT").default(3000).asPortNumber(),
   NODE_ENV: get("NODE_ENV").default("development").asString(),
   DISCORD_WEBHOOK_URL: get("DISCORD_WEBHOOK_URL").required().asString(),
+  SECRET_TOKEN: get("SECRET_TOKEN").required().asString(),
 };
