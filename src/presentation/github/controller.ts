7ceda07d937301ecbcea1a8c0f9301de9a19cd7f
Alex <alex@alex-dev.me>
2024-03-01 00:42:48 -0600

    Add GitHub and Discord services to GithubController

--- src/presentation/github/controller.ts
+++ src/presentation/github/controller.ts
@@ -1,15 +1,36 @@
 import { Request, Response } from "express";
+import { GitHubService } from "../services/github.service";
+import { DiscordService } from "../services/discordd.service";
 
 export class GithubController {
-  constructor() {}
+  constructor(
+    private readonly githubService: GitHubService = new GitHubService(),
+    private readonly discordService: DiscordService = new DiscordService()
+  ) {}
 
   webhookHandler = (req: Request, res: Response) => {
     const githubEvent = req.headers["x-github-event"] ?? "unknown";
     const signature = req.headers["x-hub-signature-256"] ?? "unknown";
     const { body: payload } = req;
 
-    console.log(JSON.stringify(payload));
+    let message: string = "";
 
-    res.status(202).send("Accepted!");
+    switch (githubEvent) {
+      case "star":
+        message = this.githubService.onStar(payload);
+
+        break;
+      case "issues":
+        message = this.githubService.onIssue(payload);
+        break;
+      default:
+        message = `Unknown event: ${githubEvent}`;
+        break;
+    }
+
+    this.discordService
+      .notify(message)
+      .then(() => res.status(202).send("Accepted!"))
+      .catch(() => res.status(500).json({ error: "Internal Server Error" }));
   };
 }
