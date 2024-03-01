336e4fab2474b4dc82b09d992ac0e083f1d86355
Alex <alex@alex-dev.me>
2024-03-01 00:42:53 -0600

    Add DiscordService class for sending notifications via Discord webhook

---
+++ src/presentation/services/discordd.service.ts
@@ -0,0 +1,33 @@
+import { environment } from "../../config";
+
+export class DiscordService {
+  private readonly discordWebhookUrl: string = environment.DISCORD_WEBHOOK_URL;
+
+  constructor() {}
+  async notify(message: string) {
+    const body = {
+      content: message,
+      embeds: [
+        {
+          image: {
+            url: "https://media.giphy.com/media/v1.Y2lkPTc5MGI3NjExNHB2MXFycTAzODdzbmFmeTJ3M3BkOHF2NHUybjd1N3g2OWMxbmQwbCZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/y9euf7PEoS7XeZhwC4/giphy.gif",
+          },
+        },
+      ],
+    };
+
+    const res = await fetch(this.discordWebhookUrl, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(body),
+    });
+
+    if (!res.ok) {
+      console.error(`Error: ${res.status} - ${res.statusText}`);
+      return false;
+    }
+    return true;
+  }
+}
